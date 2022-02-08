adc6318fc9aea327ce0fe5d12890dafaf4d53966
White, Otto <ow20@ic.ac.uk>
2022-02-08 20:11:58 +0000

    feat: parse the contents of this file and print ast, otherwise print error and exit 100

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,40 @@ use std::path::Path;
 use std::process::exit;
 
 fn main() {
+  // Get all arguments passed to the compiler
   let args: Vec<String> = env::args().collect();
 
+  // Ensure that a single argument was given
   if args.len() != 2 {
     println!("Error: incorrect number of arguments. ");
     // TODO: Print usage
     exit(-1);
   }
 
+  // Ensure that this argument is a path leading to file
   let source_path = &args[1];
-
   if !Path::new(source_path).exists() {
     println!("Error: file does not exist. ");
     // TODO: Print usage
     exit(-1);
   }
 
+  // Read the contents of this file
   let file = File::open(source_path).unwrap();
-
   let mut buf_reader = BufReader::new(file);
   let mut program = String::new();
   buf_reader.read_to_string(&mut program).unwrap();
 
-  // Parse the program
-  // let ast = parser::parse(program);
+  // Attempt to parse the contents of this file, if an error encountered
+  // then return it and exit with status 100
+  let ast = match parser::parse(&program) {
+    Ok(ast) => ast,
+    Err(e) => {
+      println!("{}", e);
+      exit(100);
+    }
+  };
 
-  //TODO Handle errors from parsing
-
-  // println!("ast = {:?}", ast);
+  // Print the generated abstract syntax tree
+  println!("ast = {:?}", ast);
 }
