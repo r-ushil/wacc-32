df6fac1a73659bf393768f30fce27c49da5357b0
Charlie <charlie.lidbury@icloud.com>
2022-02-09 18:27:21 +0000

    test: expression type checking unit tests

--- src/analyser/expr.rs
+++ src/analyser/expr.rs
@@ -29,9 +29,13 @@ impl HasType for Expr {
           }
         },
         UnaryOper::Ord => {
-          expected_type(symbol_table, &Type::BaseType(BaseType::Char), exp)?.clone()
+          expected_type(symbol_table, &Type::BaseType(BaseType::Char), exp)?;
+          Type::BaseType(BaseType::Int)
         }
-        UnaryOper::Chr => expected_type(symbol_table, &Type::BaseType(BaseType::Int), exp)?.clone(),
+        UnaryOper::Chr => {
+          expected_type(symbol_table, &Type::BaseType(BaseType::Int), exp)?;
+          Type::BaseType(BaseType::Char)
+        }
       },
 
       Expr::BinaryApp(exp1, op, exp2) => {
@@ -78,6 +82,10 @@ impl HasType for Expr {
 
 #[cfg(test)]
 mod tests {
+  use std::fmt::Binary;
+
+  use crate::analyser::symbol_table;
+
   use super::*;
 
   /* Defines a scope with 10 variables, each starting with prefix and ending with 0..10 */
@@ -90,7 +98,7 @@ mod tests {
   }
 
   #[test]
-  fn test_literals() {
+  fn literals() {
     let symbol_table = &SymbolTable::new();
 
     assert_eq!(
@@ -115,9 +123,244 @@ mod tests {
   }
 
   #[test]
-  fn test_ident() {
-    let mut outer = SymbolTable::new();
-    populate_symbol_table(&mut outer, "outer");
-    let _inner = outer.new_scope();
+  fn idents() {
+    let mut symbol_table = SymbolTable::new();
+    populate_symbol_table(&mut symbol_table, "var");
+
+    assert_eq!(
+      Expr::Ident(Ident(String::from("var1"))).get_type(&symbol_table),
+      Ok(Type::BaseType(BaseType::Int)),
+    );
+  }
+
+  #[test]
+  fn array_elems() {
+    let x = Ident(String::from("x"));
+    let x_type = Type::Array(Box::new(Type::BaseType(BaseType::Int)));
+
+    let mut symbol_table = SymbolTable::new();
+    symbol_table.insert(&x, x_type);
+
+    assert_eq!(
+      Expr::ArrayElem(ArrayElem(x, vec!(Expr::IntLiter(5)))).get_type(&symbol_table),
+      Ok(Type::BaseType(BaseType::Int))
+    );
+  }
+
+  #[test]
+  fn unary_apps() {
+    /* Symbol Table */
+    let symbol_table = &mut SymbolTable::new();
+
+    /* BANG */
+    /* !false: Bool */
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Bang, Box::new(Expr::BoolLiter(false))).get_type(symbol_table),
+      Ok(Type::BaseType(BaseType::Bool))
+    );
+
+    /* !'a': ERROR */
+    assert!(
+      Expr::UnaryApp(UnaryOper::Bang, Box::new(Expr::CharLiter('a')))
+        .get_type(symbol_table)
+        .is_err()
+    );
+
+    /* NEG */
+    /* -5: Int */
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::IntLiter(5))).get_type(symbol_table),
+      Ok(Type::BaseType(BaseType::Int))
+    );
+
+    /* -false: ERROR*/
+    assert!(
+      Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::BoolLiter(false)))
+        .get_type(symbol_table)
+        .is_err()
+    );
+
+    /* LEN */
+    /* len [1,2,3]: Int */
+    let x = Ident(String::from("x"));
+    let x_type = Type::Array(Box::new(Type::BaseType(BaseType::Int)));
+    symbol_table.insert(&x, x_type);
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Len, Box::new(Expr::Ident(x))).get_type(symbol_table),
+      Ok(Type::BaseType(BaseType::Int))
+    );
+
+    /* len 5: ERROR */
+    assert!(Expr::UnaryApp(UnaryOper::Len, Box::new(Expr::IntLiter(5)))
+      .get_type(symbol_table)
+      .is_err());
+
+    /* ORD */
+    /* ord 'a': Int */
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Ord, Box::new(Expr::CharLiter('a'))).get_type(symbol_table),
+      Ok(Type::BaseType(BaseType::Int))
+    );
+
+    /* ord 5: ERROR */
+    assert!(Expr::UnaryApp(UnaryOper::Ord, Box::new(Expr::IntLiter(5)))
+      .get_type(symbol_table)
+      .is_err());
+
+    /* CHR */
+    /* chr 5: Char */
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Chr, Box::new(Expr::IntLiter(5))).get_type(symbol_table),
+      Ok(Type::BaseType(BaseType::Char))
+    );
+
+    /* chr 'a': ERROR */
+    assert!(
+      Expr::UnaryApp(UnaryOper::Chr, Box::new(Expr::CharLiter('a')))
+        .get_type(symbol_table)
+        .is_err()
+    );
+  }
+
+  #[test]
+  fn binary_apps() {
+    let symbol_table = &mut SymbolTable::new();
+
+    /* 5 + false: ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::IntLiter(5)),
+      BinaryOper::Add,
+      Box::new(Expr::BoolLiter(false))
+    )
+    .get_type(symbol_table)
+    .is_err());
+
+    /* 5 * 'a': ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::IntLiter(5)),
+      BinaryOper::Mul,
+      Box::new(Expr::CharLiter('a'))
+    )
+    .get_type(symbol_table)
+    .is_err());
+
+    /* false / "hello": ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::BoolLiter(false)),
+      BinaryOper::Div,
+      Box::new(Expr::StrLiter(String::from("hello")))
+    )
+    .get_type(symbol_table)
+    .is_err());
+
+    /* false && 6: ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::BoolLiter(false)),
+      BinaryOper::And,
+      Box::new(Expr::IntLiter(6))
+    )
+    .get_type(symbol_table)
+    .is_err());
+
+    /* MATH CAN BE DONE ON INTS. */
+    /* 5 * 5: Int */
+    assert_eq!(
+      Expr::BinaryApp(
+        Box::new(Expr::IntLiter(5)),
+        BinaryOper::Mul,
+        Box::new(Expr::IntLiter(5)),
+      )
+      .get_type(symbol_table),
+      Ok(Type::BaseType(BaseType::Int)),
+    );
+
+    /* 5 + 5: Int */
+    assert_eq!(
+      Expr::BinaryApp(
+        Box::new(Expr::IntLiter(5)),
+        BinaryOper::Add,
+        Box::new(Expr::IntLiter(5)),
+      )
+      .get_type(symbol_table),
+      Ok(Type::BaseType(BaseType::Int)),
+    );
+
+    /* MATH CANT BE DONE ON ANYTHING ELSE */
+    /* 'a' + 'b': ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::CharLiter('a')),
+      BinaryOper::Add,
+      Box::new(Expr::CharLiter('b'))
+    )
+    .get_type(symbol_table)
+    .is_err());
+
+    /* false + false: ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::BoolLiter(false)),
+      BinaryOper::Add,
+      Box::new(Expr::BoolLiter(false))
+    )
+    .get_type(symbol_table)
+    .is_err());
+
+    /* Any type can be comapred to itself. */
+    for expr in vec![
+      Expr::IntLiter(5),
+      Expr::BoolLiter(false),
+      Expr::StrLiter(String::from("hello")),
+      Expr::PairLiter,
+      Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::IntLiter(5))),
+      Expr::BinaryApp(
+        Box::new(Expr::StrLiter(String::from("hello world"))),
+        BinaryOper::Eq,
+        Box::new(Expr::StrLiter(String::from("hello steve"))),
+      ),
+    ] {
+      for oper in vec![
+        BinaryOper::Gt,
+        BinaryOper::Gte,
+        BinaryOper::Lt,
+        BinaryOper::Lte,
+        BinaryOper::Eq,
+        BinaryOper::Neq,
+      ] {
+        assert_eq!(
+          Expr::BinaryApp(Box::new(expr.clone()), oper, Box::new(expr.clone()))
+            .get_type(symbol_table),
+          Ok(Type::BaseType(BaseType::Bool))
+        );
+      }
+    }
+
+    /* Boolean logic can only be applied to booleans */
+    /* 5 && 5: ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::IntLiter(5)),
+      BinaryOper::And,
+      Box::new(Expr::IntLiter(5)),
+    )
+    .get_type(symbol_table)
+    .is_err());
+
+    /* 'a' || 'a': ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::CharLiter('a')),
+      BinaryOper::Or,
+      Box::new(Expr::CharLiter('a')),
+    )
+    .get_type(symbol_table)
+    .is_err());
+
+    /* true && true: bool */
+    assert_eq!(
+      Expr::BinaryApp(
+        Box::new(Expr::BoolLiter(true)),
+        BinaryOper::And,
+        Box::new(Expr::BoolLiter(true)),
+      )
+      .get_type(symbol_table),
+      Ok(Type::BaseType(BaseType::Bool))
+    );
   }
 }
