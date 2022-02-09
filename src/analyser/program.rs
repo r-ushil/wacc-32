a4e4b82641e765c5bd2455cecdb8421258dc2d3d
Charlie <charlie.lidbury@icloud.com>
2022-02-09 11:19:20 +0000

    refactor: formatting and warning suppression

--- src/analyser/program.rs
+++ src/analyser/program.rs
@@ -3,6 +3,7 @@ use crate::ast::*;
 
 /* Semantically checks an entire program. */
 /* This function initialises the symbol table and function table. */
+#[allow(dead_code)]
 fn program(_program: &Program) -> AResult<()> {
   todo!();
 }
--- src/analyser/stat.rs
+++ src/analyser/stat.rs
@@ -1,8 +1,4 @@
-use super::{
-  equal_types, expected_type,
-  symbol_table::{self, SymbolTable},
-  AResult, HasType,
-};
+use super::{equal_types, expected_type, symbol_table::SymbolTable, AResult, HasType};
 use crate::ast::*;
 
 impl HasType for AssignLhs {
@@ -15,6 +11,7 @@ impl HasType for AssignLhs {
   }
 }
 
+#[allow(unused_variables)]
 impl HasType for AssignRhs {
   fn get_type(&self, symbol_table: &SymbolTable) -> AResult<Type> {
     Ok(match self {
@@ -32,7 +29,14 @@ impl HasType for AssignRhs {
 impl HasType for PairElem {
   fn get_type(&self, symbol_table: &SymbolTable) -> AResult<Type> {
     Ok(match self {
-      PairElem::Fst(exp) | PairElem::Snd(exp) => exp.get_type(symbol_table)?,
+      PairElem::Fst(p) => match p.get_type(symbol_table)? {
+        Type::Pair(left, _) => *left,
+        t => return Err(format!("TYPE ERROR:\n\tExpected: Pair\n\tActual:{:?}", t)),
+      },
+      PairElem::Snd(p) => match p.get_type(symbol_table)? {
+        Type::Pair(_, right) => *right,
+        t => return Err(format!("TYPE ERROR:\n\tExpected: Pair\n\tActual:{:?}", t)),
+      },
     })
   }
 }
@@ -58,6 +62,7 @@ impl HasType for ArrayLiter {
 Declarations will add to the symbol table.
 Scopes will make a new scope within the symbol table.
 If the statment ALWAYS returns with the same type, returns that type. */
+#[allow(dead_code)]
 fn stat(symbol_table: &mut SymbolTable, statement: &Stat) -> AResult<Option<Type>> {
   /* Returns error if there is any. */
   match statement {
@@ -67,13 +72,13 @@ fn stat(symbol_table: &mut SymbolTable, statement: &Stat) -> AResult<Option<Type
       symbol_table.insert(id, expected.clone());
 
       expected_type(symbol_table, &expected, val)?;
-    },
+    }
     Stat::Assignment(lhs, rhs) => {
       equal_types(symbol_table, lhs, rhs)?;
-    }, // x = [1,2,3]
+    } // x = [1,2,3]
     Stat::Read(dest) => {
       expected_type(symbol_table, &Type::BaseType(BaseType::String), dest)?;
-    },
+    }
     Stat::Free(expr) => match expr.get_type(symbol_table)? {
       Type::Pair(_, _) | Type::Array(_) => (),
       actual_type => {
@@ -81,17 +86,17 @@ fn stat(symbol_table: &mut SymbolTable, statement: &Stat) -> AResult<Option<Type
           "TYPE ERROR: Expected Type\n\tExpected: Pair or Array\n\tActual:{:?}",
           actual_type
         ))
-      },
+      }
     },
     Stat::Return(expr) => {
       return Ok(Some(expr.get_type(symbol_table)?));
-    },
+    }
     Stat::Exit(expr) => {
       expected_type(symbol_table, &Type::BaseType(BaseType::Int), expr)?;
-    },
+    }
     Stat::Print(expr) | Stat::Println(expr) => {
       expected_type(symbol_table, &Type::BaseType(BaseType::String), expr)?;
-    },
+    }
     Stat::If(cond, if_stat, else_stat) => {
       expected_type(symbol_table, &Type::BaseType(BaseType::Bool), cond)?;
 
@@ -102,19 +107,19 @@ fn stat(symbol_table: &mut SymbolTable, statement: &Stat) -> AResult<Option<Type
           return Ok(Some(a));
         }
       }
-    },
+    }
     Stat::While(cond, body) => {
       expected_type(symbol_table, &Type::BaseType(BaseType::Bool), cond)?;
 
       /* Even if body does return, statement can't be guarenteed to return
       because the body might not run. */
       stat(symbol_table, body)?;
-    },
+    }
     Stat::Scope(body) => return stat(symbol_table, body),
     Stat::Sequence(fst, snd) => {
       /* NOTE: if x always returns in x;y, y will not get type checked. */
       return stat(symbol_table, fst).or(stat(symbol_table, snd));
-    },
+    }
   }
 
   /* Returns with Ok otherwise. */
@@ -125,5 +130,12 @@ fn stat(symbol_table: &mut SymbolTable, statement: &Stat) -> AResult<Option<Type
 mod tests {
 
   #[test]
-  fn test_pair_elem_has_type() {}
+  fn test_pair_elem_has_type() {
+    // todo!();
+  }
+
+  #[test]
+  fn test_array_lit_has_type() {
+    // todo!();
+  }
 }
