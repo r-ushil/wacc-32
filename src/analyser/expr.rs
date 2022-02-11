217ee52f2d205e46a8da4961cc21faaa8a23e7fa
Charlie <charlie.lidbury@icloud.com>
2022-02-11 18:42:15 +0000

    test: expression unit tests updated

--- src/analyser/expr.rs
+++ src/analyser/expr.rs
@@ -102,272 +102,284 @@ mod tests {
 
   /* Defines a scope with 10 variables, each starting with prefix and ending
    * with 0..10 */
-  // fn populate_context(context: &mut Context, prefix: &str) {
-  //   for i in 0..10 {
-  //     let ident = format!("{}{}", prefix, i);
-  //     let t = Type::Int;
-  //     context.insert(&ident, t);
-  //   }
-  // }
+  fn populate_context(context: &mut Context, prefix: &str) {
+    for i in 0..10 {
+      let ident = format!("{}{}", prefix, i);
+      let t = Type::Int;
+      context.insert(&ident, t);
+    }
+  }
 
-  // #[test]
-  // fn literals() {
-  //   let context = &Context::new();
+  #[test]
+  fn literals() {
+    let context = &Context::new();
 
-  //   assert_eq!(Expr::IntLiter(5).get_type(context), Ok(Type::Int));
-  //   assert_eq!(Expr::BoolLiter(false).get_type(context), Ok(Type::Bool));
-  //   assert_eq!(Expr::CharLiter('a').get_type(context), Ok(Type::Char));
-  //   assert_eq!(
-  //     Expr::PairLiter.get_type(context),
-  //     Ok(Type::Pair(Box::new(Type::Any), Box::new(Type::Any))),
-  //   );
-  // }
+    assert_eq!(
+      Expr::IntLiter(5).get_type(context, &mut vec!()),
+      Some(Type::Int)
+    );
+    assert_eq!(
+      Expr::BoolLiter(false).get_type(context, &mut vec!()),
+      Some(Type::Bool)
+    );
+    assert_eq!(
+      Expr::CharLiter('a').get_type(context, &mut vec!()),
+      Some(Type::Char)
+    );
+    assert_eq!(
+      Expr::PairLiter.get_type(context, &mut vec!()),
+      Some(Type::Pair(Box::new(Type::Any), Box::new(Type::Any))),
+    );
+  }
 
-  // #[test]
-  // fn idents() {
-  //   let mut context = Context::new();
-  //   populate_context(&mut context, "var");
+  #[test]
+  fn idents() {
+    let mut context = Context::new();
+    populate_context(&mut context, "var");
 
-  //   assert_eq!(
-  //     Expr::Ident(String::from("var1")).get_type(&context),
-  //     Ok(Type::Int),
-  //   );
-  // }
+    assert_eq!(
+      Expr::Ident(String::from("var1")).get_type(&context, &mut vec![]),
+      Some(Type::Int),
+    );
+  }
 
-  // #[test]
-  // fn array_elems() {
-  //   let x = String::from("x");
-  //   let x_type = Type::Array(Box::new(Type::Int));
+  #[test]
+  fn array_elems() {
+    let x = String::from("x");
+    let x_type = Type::Array(Box::new(Type::Int));
 
-  //   let mut context = Context::new();
-  //   context.insert(&x, x_type);
+    let mut context = Context::new();
+    context.insert(&x, x_type);
 
-  //   assert_eq!(
-  //     Expr::ArrayElem(ArrayElem(x, vec!(Expr::IntLiter(5)))).get_type(&context),
-  //     Ok(Type::Int)
-  //   );
-  // }
+    assert_eq!(
+      Expr::ArrayElem(ArrayElem(x, vec!(Expr::IntLiter(5)))).get_type(&context, &mut vec![]),
+      Some(Type::Int)
+    );
+  }
 
-  // #[test]
-  // fn unary_apps() {
-  //   /* Symbol Table */
-  //   let context = &mut Context::new();
+  #[test]
+  fn unary_apps() {
+    /* Symbol Table */
+    let context = &mut Context::new();
 
-  //   /* BANG */
-  //   /* !false: Bool */
-  //   assert_eq!(
-  //     Expr::UnaryApp(UnaryOper::Bang, Box::new(Expr::BoolLiter(false))).get_type(context),
-  //     Ok(Type::Bool)
-  //   );
+    /* BANG */
+    /* !false: Bool */
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Bang, Box::new(Expr::BoolLiter(false)))
+        .get_type(context, &mut vec!()),
+      Some(Type::Bool)
+    );
 
-  //   /* !'a': ERROR */
-  //   assert!(
-  //     Expr::UnaryApp(UnaryOper::Bang, Box::new(Expr::CharLiter('a')))
-  //       .get_type(context)
-  //       .is_err()
-  //   );
+    /* !'a': ERROR */
+    assert!(
+      Expr::UnaryApp(UnaryOper::Bang, Box::new(Expr::CharLiter('a')))
+        .get_type(context, &mut vec!())
+        .is_none()
+    );
 
-  //   /* NEG */
-  //   /* -5: Int */
-  //   assert_eq!(
-  //     Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::IntLiter(5))).get_type(context),
-  //     Ok(Type::Int)
-  //   );
+    /* NEG */
+    /* -5: Int */
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::IntLiter(5))).get_type(context, &mut vec!()),
+      Some(Type::Int)
+    );
 
-  //   /* -false: ERROR */
-  //   assert!(
-  //     Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::BoolLiter(false)))
-  //       .get_type(context)
-  //       .is_err()
-  //   );
+    /* -false: ERROR */
+    assert!(
+      Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::BoolLiter(false)))
+        .get_type(context, &mut vec!())
+        .is_none()
+    );
 
-  //   /* LEN */
-  //   /* len [1,2,3]: Int */
-  //   let x = String::from("x");
-  //   let x_type = Type::Array(Box::new(Type::Int));
-  //   context.insert(&x, x_type);
-  //   assert_eq!(
-  //     Expr::UnaryApp(UnaryOper::Len, Box::new(Expr::Ident(x))).get_type(context),
-  //     Ok(Type::Int)
-  //   );
+    /* LEN */
+    /* len [1,2,3]: Int */
+    let x = String::from("x");
+    let x_type = Type::Array(Box::new(Type::Int));
+    context.insert(&x, x_type);
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Len, Box::new(Expr::Ident(x))).get_type(context, &mut vec!()),
+      Some(Type::Int)
+    );
 
-  //   /* len 5: ERROR */
-  //   assert!(Expr::UnaryApp(UnaryOper::Len, Box::new(Expr::IntLiter(5)))
-  //     .get_type(context)
-  //     .is_err());
+    /* len 5: ERROR */
+    assert!(Expr::UnaryApp(UnaryOper::Len, Box::new(Expr::IntLiter(5)))
+      .get_type(context, &mut vec!())
+      .is_none());
 
-  //   /* ORD */
-  //   /* ord 'a': Int */
-  //   assert_eq!(
-  //     Expr::UnaryApp(UnaryOper::Ord, Box::new(Expr::CharLiter('a'))).get_type(context),
-  //     Ok(Type::Int)
-  //   );
+    /* ORD */
+    /* ord 'a': Int */
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Ord, Box::new(Expr::CharLiter('a'))).get_type(context, &mut vec!()),
+      Some(Type::Int)
+    );
 
-  //   /* ord 5: ERROR */
-  //   assert!(Expr::UnaryApp(UnaryOper::Ord, Box::new(Expr::IntLiter(5)))
-  //     .get_type(context)
-  //     .is_err());
+    /* ord 5: ERROR */
+    assert!(Expr::UnaryApp(UnaryOper::Ord, Box::new(Expr::IntLiter(5)))
+      .get_type(context, &mut vec!())
+      .is_none());
 
-  //   /* CHR */
-  //   /* chr 5: Char */
-  //   assert_eq!(
-  //     Expr::UnaryApp(UnaryOper::Chr, Box::new(Expr::IntLiter(5))).get_type(context),
-  //     Ok(Type::Char)
-  //   );
+    /* CHR */
+    /* chr 5: Char */
+    assert_eq!(
+      Expr::UnaryApp(UnaryOper::Chr, Box::new(Expr::IntLiter(5))).get_type(context, &mut vec!()),
+      Some(Type::Char)
+    );
 
-  //   /* chr 'a': ERROR */
-  //   assert!(
-  //     Expr::UnaryApp(UnaryOper::Chr, Box::new(Expr::CharLiter('a')))
-  //       .get_type(context)
-  //       .is_err()
-  //   );
-  // }
+    /* chr 'a': ERROR */
+    assert!(
+      Expr::UnaryApp(UnaryOper::Chr, Box::new(Expr::CharLiter('a')))
+        .get_type(context, &mut vec!())
+        .is_none()
+    );
+  }
 
-  // #[test]
-  // fn binary_apps() {
-  //   let context = &mut Context::new();
+  #[test]
+  fn binary_apps() {
+    let context = &mut Context::new();
 
-  //   /* 5 + false: ERROR */
-  //   assert!(Expr::BinaryApp(
-  //     Box::new(Expr::IntLiter(5)),
-  //     BinaryOper::Add,
-  //     Box::new(Expr::BoolLiter(false))
-  //   )
-  //   .get_type(context)
-  //   .is_err());
+    /* 5 + false: ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::IntLiter(5)),
+      BinaryOper::Add,
+      Box::new(Expr::BoolLiter(false))
+    )
+    .get_type(context, &mut vec!())
+    .is_some());
 
-  //   /* 5 * 'a': ERROR */
-  //   assert!(Expr::BinaryApp(
-  //     Box::new(Expr::IntLiter(5)),
-  //     BinaryOper::Mul,
-  //     Box::new(Expr::CharLiter('a'))
-  //   )
-  //   .get_type(context)
-  //   .is_err());
+    /* 5 * 'a': ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::IntLiter(5)),
+      BinaryOper::Mul,
+      Box::new(Expr::CharLiter('a'))
+    )
+    .get_type(context, &mut vec!())
+    .is_some());
 
-  //   /* false / "hello": ERROR */
-  //   assert!(Expr::BinaryApp(
-  //     Box::new(Expr::BoolLiter(false)),
-  //     BinaryOper::Div,
-  //     Box::new(Expr::StrLiter(String::from("hello")))
-  //   )
-  //   .get_type(context)
-  //   .is_err());
+    /* false / "hello": ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::BoolLiter(false)),
+      BinaryOper::Div,
+      Box::new(Expr::StrLiter(String::from("hello")))
+    )
+    .get_type(context, &mut vec!())
+    .is_some());
 
-  //   /* false && 6: ERROR */
-  //   assert!(Expr::BinaryApp(
-  //     Box::new(Expr::BoolLiter(false)),
-  //     BinaryOper::And,
-  //     Box::new(Expr::IntLiter(6))
-  //   )
-  //   .get_type(context)
-  //   .is_err());
+    /* false && 6: ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::BoolLiter(false)),
+      BinaryOper::And,
+      Box::new(Expr::IntLiter(6))
+    )
+    .get_type(context, &mut vec!())
+    .is_some());
 
-  //   /* MATH CAN BE DONE ON INTS. */
-  //   /* 5 * 5: Int */
-  //   assert_eq!(
-  //     Expr::BinaryApp(
-  //       Box::new(Expr::IntLiter(5)),
-  //       BinaryOper::Mul,
-  //       Box::new(Expr::IntLiter(5)),
-  //     )
-  //     .get_type(context),
-  //     Ok(Type::Int),
-  //   );
+    /* MATH CAN BE DONE ON INTS. */
+    /* 5 * 5: Int */
+    assert_eq!(
+      Expr::BinaryApp(
+        Box::new(Expr::IntLiter(5)),
+        BinaryOper::Mul,
+        Box::new(Expr::IntLiter(5)),
+      )
+      .get_type(context, &mut vec!()),
+      Some(Type::Int),
+    );
 
-  //   /* 5 + 5: Int */
-  //   assert_eq!(
-  //     Expr::BinaryApp(
-  //       Box::new(Expr::IntLiter(5)),
-  //       BinaryOper::Add,
-  //       Box::new(Expr::IntLiter(5)),
-  //     )
-  //     .get_type(context),
-  //     Ok(Type::Int),
-  //   );
+    /* 5 + 5: Int */
+    assert_eq!(
+      Expr::BinaryApp(
+        Box::new(Expr::IntLiter(5)),
+        BinaryOper::Add,
+        Box::new(Expr::IntLiter(5)),
+      )
+      .get_type(context, &mut vec!()),
+      Some(Type::Int),
+    );
 
-  //   /* MATH CANT BE DONE ON ANYTHING ELSE */
-  //   /* 'a' + 'b': ERROR */
-  //   assert!(Expr::BinaryApp(
-  //     Box::new(Expr::CharLiter('a')),
-  //     BinaryOper::Add,
-  //     Box::new(Expr::CharLiter('b'))
-  //   )
-  //   .get_type(context)
-  //   .is_err());
+    /* MATH CANT BE DONE ON ANYTHING ELSE */
+    /* 'a' + 'b': ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::CharLiter('a')),
+      BinaryOper::Add,
+      Box::new(Expr::CharLiter('b'))
+    )
+    .get_type(context, &mut vec!())
+    .is_some());
 
-  //   /* false + false: ERROR */
-  //   assert!(Expr::BinaryApp(
-  //     Box::new(Expr::BoolLiter(false)),
-  //     BinaryOper::Add,
-  //     Box::new(Expr::BoolLiter(false))
-  //   )
-  //   .get_type(context)
-  //   .is_err());
+    /* false + false: ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::BoolLiter(false)),
+      BinaryOper::Add,
+      Box::new(Expr::BoolLiter(false))
+    )
+    .get_type(context, &mut vec!())
+    .is_some());
 
-  //   /* Any type can be comapred to itself. */
-  //   for expr in vec![
-  //     Expr::IntLiter(5),
-  //     Expr::BoolLiter(false),
-  //     Expr::StrLiter(String::from("hello")),
-  //     Expr::PairLiter,
-  //     Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::IntLiter(5))),
-  //     Expr::BinaryApp(
-  //       Box::new(Expr::StrLiter(String::from("hello world"))),
-  //       BinaryOper::Eq,
-  //       Box::new(Expr::StrLiter(String::from("hello steve"))),
-  //     ),
-  //   ] {
-  //     for oper in vec![BinaryOper::Eq, BinaryOper::Neq] {
-  //       assert_eq!(
-  //         Expr::BinaryApp(Box::new(expr.clone()), oper, Box::new(expr.clone())).get_type(context),
-  //         Ok(Type::Bool)
-  //       );
-  //     }
-  //   }
+    /* Any type can be comapred to itself. */
+    for expr in vec![
+      Expr::IntLiter(5),
+      Expr::BoolLiter(false),
+      Expr::StrLiter(String::from("hello")),
+      Expr::PairLiter,
+      Expr::UnaryApp(UnaryOper::Neg, Box::new(Expr::IntLiter(5))),
+      Expr::BinaryApp(
+        Box::new(Expr::StrLiter(String::from("hello world"))),
+        BinaryOper::Eq,
+        Box::new(Expr::StrLiter(String::from("hello steve"))),
+      ),
+    ] {
+      for oper in vec![BinaryOper::Eq, BinaryOper::Neq] {
+        assert_eq!(
+          Expr::BinaryApp(Box::new(expr.clone()), oper, Box::new(expr.clone()))
+            .get_type(context, &mut vec!()),
+          Some(Type::Bool)
+        );
+      }
+    }
 
-  //   for expr in vec![Expr::IntLiter(5), Expr::CharLiter('a')] {
-  //     for oper in vec![
-  //       BinaryOper::Gt,
-  //       BinaryOper::Gte,
-  //       BinaryOper::Lt,
-  //       BinaryOper::Lte,
-  //     ] {
-  //       assert_eq!(
-  //         Expr::BinaryApp(Box::new(expr.clone()), oper, Box::new(expr.clone())).get_type(context),
-  //         Ok(Type::Bool)
-  //       );
-  //     }
-  //   }
+    for expr in vec![Expr::IntLiter(5), Expr::CharLiter('a')] {
+      for oper in vec![
+        BinaryOper::Gt,
+        BinaryOper::Gte,
+        BinaryOper::Lt,
+        BinaryOper::Lte,
+      ] {
+        assert_eq!(
+          Expr::BinaryApp(Box::new(expr.clone()), oper, Box::new(expr.clone()))
+            .get_type(context, &mut vec!()),
+          Some(Type::Bool)
+        );
+      }
+    }
 
-  //   /* Boolean logic can only be applied to booleans */
-  //   /* 5 && 5: ERROR */
-  //   assert!(Expr::BinaryApp(
-  //     Box::new(Expr::IntLiter(5)),
-  //     BinaryOper::And,
-  //     Box::new(Expr::IntLiter(5)),
-  //   )
-  //   .get_type(context)
-  //   .is_err());
+    /* Boolean logic can only be applied to booleans */
+    /* 5 && 5: ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::IntLiter(5)),
+      BinaryOper::And,
+      Box::new(Expr::IntLiter(5)),
+    )
+    .get_type(context, &mut vec!())
+    .is_some());
 
-  //   /* 'a' || 'a': ERROR */
-  //   assert!(Expr::BinaryApp(
-  //     Box::new(Expr::CharLiter('a')),
-  //     BinaryOper::Or,
-  //     Box::new(Expr::CharLiter('a')),
-  //   )
-  //   .get_type(context)
-  //   .is_err());
+    /* 'a' || 'a': ERROR */
+    assert!(Expr::BinaryApp(
+      Box::new(Expr::CharLiter('a')),
+      BinaryOper::Or,
+      Box::new(Expr::CharLiter('a')),
+    )
+    .get_type(context, &mut vec!())
+    .is_some());
 
-  //   /* true && true: bool */
-  //   assert_eq!(
-  //     Expr::BinaryApp(
-  //       Box::new(Expr::BoolLiter(true)),
-  //       BinaryOper::And,
-  //       Box::new(Expr::BoolLiter(true)),
-  //     )
-  //     .get_type(context),
-  //     Ok(Type::Bool)
-  //   );
-  // }
+    /* true && true: bool */
+    assert_eq!(
+      Expr::BinaryApp(
+        Box::new(Expr::BoolLiter(true)),
+        BinaryOper::And,
+        Box::new(Expr::BoolLiter(true)),
+      )
+      .get_type(context, &mut vec!()),
+      Some(Type::Bool)
+    );
+  }
 }
