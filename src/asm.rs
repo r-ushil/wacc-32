879709cc8b7cb6ac29ce5b4865e9527d2d9a5c34
rp620 <rushil.patel20@imperial.ac.uk>
2022-02-17 18:08:34 +0000

    feat: abstracted condition code for branch and move instructions

--- src/asm.rs
+++ src/asm.rs
@@ -15,6 +15,50 @@ impl Display for Op2 {
     }
   }
 }
+
+impl Display for CondCode {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      CondCode::EQ => write!(f, "EQ"),
+      CondCode::NE => write!(f, "NE"),
+      CondCode::CS => write!(f, "CS"),
+      CondCode::HS => write!(f, "HS"),
+      CondCode::CC => write!(f, "CC"),
+      CondCode::LO => write!(f, "LO"),
+      CondCode::MI => write!(f, "MI"),
+      CondCode::PL => write!(f, "PL"),
+      CondCode::VS => write!(f, "VS"),
+      CondCode::VC => write!(f, "VC"),
+      CondCode::HI => write!(f, "HI"),
+      CondCode::LS => write!(f, "LS"),
+      CondCode::GE => write!(f, "GE"),
+      CondCode::LT => write!(f, "LT"),
+      CondCode::GT => write!(f, "GT"),
+      CondCode::LE => write!(f, "LE"),
+      CondCode::AL => write!(f, ""),
+    }
+  }
+}
+pub enum CondCode {
+  EQ,
+  NE,
+  CS,
+  HS,
+  CC,
+  LO,
+  MI,
+  PL,
+  VS,
+  VC,
+  HI,
+  LS,
+  GE,
+  LT,
+  GT,
+  LE,
+  AL,
+}
+
 pub enum Op2 {
   Reg(Reg),
   Imm(Imm),
@@ -51,9 +95,9 @@ pub struct MemAddress {
 
 pub enum Instr {
   LoadImm(Reg, Imm),
-  Mov(Reg, Op2),
+  Mov(Reg, Op2, CondCode),
 
-  Branch(Branch),
+  Branch(Branch, CondCode),
   Pop,
   Assemble, //.ltorg
 
@@ -61,28 +105,19 @@ pub enum Instr {
   Store(Reg, MemAddress),
   LoadMemByte(Reg, MemAddress),
 
-  Add(Reg, Reg, Op2),
-  Sub(Reg, Reg, Op2),
-
   And(Reg, Reg, Op2),
   Or(Reg, Reg, Op2),
 
   Cmp(Reg, Op2),
-  MovEq(Reg, Op2), //moves depending on flag
-  MovNe(Reg, Op2),
-  MovGe(Reg, Op2),
-  MovLt(Reg, Op2),
-  MovGt(Reg, Op2),
-  MovLe(Reg, Op2),
 
-  BranchOverflow(Branch),
+  Add(Reg, Reg, Op2),
+  Sub(Reg, Reg, Op2),
 
   AddFlags(Reg, Reg, Op2),
   SubFlags(Reg, Reg, Op2),
 
   Multiply(Reg, Reg, Reg, Reg),
 
-  BranchNotEqual(Branch),
   ReverseSubtract(Reg, Reg, Op2),
 }
 
@@ -90,30 +125,29 @@ impl Display for Instr {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     match self {
       Instr::LoadImm(reg, val) => write!(f, "LDR {}, ={}", reg, val),
-      Instr::Mov(reg, op2) => write!(f, "MOV {}, {}", reg, op2),
-      Instr::Branch(branch) => write!(f, "BL {}", branch),
+
       Instr::Pop => write!(f, "POP {{pc}}"),
       Instr::Assemble => write!(f, ".ltorg"),
+
       Instr::StoreByte(reg, mem_addr) => write!(f, "STRB {}, {}", reg, mem_addr),
-      Instr::Store(reg, mem_addr) => write!(f, "STRB {}, {}", reg, mem_addr),
+      Instr::Store(reg, mem_addr) => write!(f, "STR {}, {}", reg, mem_addr),
+
       Instr::LoadMemByte(reg, mem_addr) => write!(f, "LDRSB {}, {}", reg, mem_addr),
+
       Instr::Add(r1, r2, op2) => write!(f, "ADD {}, {}, {}", r1, r2, op2),
       Instr::Sub(r1, r2, op2) => write!(f, "SUB {}, {}, {}", r1, r2, op2),
+      Instr::AddFlags(r1, r2, op2) => write!(f, "ADDS {}, {}, {}", r1, r2, op2),
+      Instr::SubFlags(r1, r2, op2) => write!(f, "SUBS {}, {}, {}", r1, r2, op2),
+
       Instr::And(r1, r2, op2) => write!(f, "AND {}, {}, {}", r1, r2, op2),
       Instr::Or(r1, r2, op2) => write!(f, "ORR {}, {}, {}", r1, r2, op2),
       Instr::Cmp(reg, op2) => write!(f, "CMP {}, {}", reg, op2),
-      Instr::MovEq(reg, op2) => write!(f, "MOVEQ {}, {}", reg, op2),
-      Instr::MovNe(reg, op2) => write!(f, "MOVNE {}, {}", reg, op2),
-      Instr::MovGe(reg, op2) => write!(f, "MOVGE {}, {}", reg, op2),
-      Instr::MovLt(reg, op2) => write!(f, "MOVLT {}, {}", reg, op2),
-      Instr::MovGt(reg, op2) => write!(f, "MOVGT {}, {}", reg, op2),
-      Instr::MovLe(reg, op2) => write!(f, "MOVLE {}, {}", reg, op2),
-      Instr::BranchOverflow(branch) => write!(f, "BLVS {}", branch),
-      Instr::AddFlags(r1, r2, op2) => write!(f, "ADDS {}, {}, {}", r1, r2, op2),
-      Instr::SubFlags(r1, r2, op2) => write!(f, "SUBS {}, {}, {}", r1, r2, op2),
+
+      Instr::Mov(reg, op2, code) => write!(f, "MOV{} {}, {}", code, reg, op2),
+      Instr::Branch(branch, code) => write!(f, "BL{} {}", code, branch),
+
       Instr::Multiply(r1, r2, r3, r4) => write!(f, "SMULL {}, {}, {}, {}", r1, r2, r3, r4),
       Instr::ReverseSubtract(r1, r2, op2) => write!(f, "RSBS {}, {}, {}", r1, r2, op2),
-      Instr::BranchNotEqual(branch) => write!(f, "BLNE {}", branch),
     }
   }
 }
