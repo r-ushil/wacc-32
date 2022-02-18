8898555a5eee64ecb84e9088a8237ddda72cb33d
Charlie <charlie.lidbury@icloud.com>
2022-02-18 00:12:28 +0000

    test: if_statement

--- src/generator/stat.rs
+++ src/generator/stat.rs
@@ -19,8 +19,41 @@ impl Generatable for ArrayLiter {
 }
 
 impl Generatable for Stat {
-  // fn generate(&self, _code: &mut Vec<Instr>, _registers: &[Reg]) {}
+  // fn generate(&self, code: &mut Vec<Instr>, registers: &[Reg]) {}
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+  use super::*;
+
+  #[test]
+  fn if_statement() {
+    let cond = Expr::BoolLiter(true); // true
+    let true_body = Stat::Println(Expr::StrLiter(String::from("True Body"))); // println "True Body"
+    let false_body = Stat::Println(Expr::StrLiter(String::from("False Body"))); // println "False Body"
+
+    let if_statement = Stat::If(
+      // if
+      cond.clone(),                 // true
+      Box::new(true_body.clone()),  // then println "True Body"
+      Box::new(false_body.clone()), // else println "False Body"
+    ); // fi
+
+    let registers = &vec![];
+
+    let actual_code = &mut vec![];
+    if_statement.generate(actual_code, registers);
+
+    let expected_code = &mut vec![];
+    cond.generate(expected_code, registers);
+    expected_code.push(Instr::Cmp(Reg::RegNum(4), Op2::Imm(0))); // CMP r4, #0
+    expected_code.push(Instr::Branch(String::from("L0"), CondCode::EQ)); // BEQ L0
+    true_body.generate(expected_code, registers);
+    expected_code.push(Instr::Branch(String::from("L1"), CondCode::AL)); // B L1
+    expected_code.push(Instr::Label(String::from("L0"))); // LO:
+    false_body.generate(expected_code, registers);
+    expected_code.push(Instr::Label(String::from("L1"))); // LO:
+
+    assert_eq!(actual_code, expected_code);
+  }
+}
