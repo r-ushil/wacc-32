8cd77f08ce0f341b43acc5ed2bd3e1536d80bc07
Charlie <charlie.lidbury@icloud.com>
2022-02-24 14:28:56 +0000

    refactor: re-ordered display impls to order in asm.rs

--- src/generator/display.rs
+++ src/generator/display.rs
@@ -28,6 +28,115 @@ impl Display for Asm {
   }
 }
 
+impl Display for Directive {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    use Directive::*;
+    match self {
+      Text => write!(f, ".text"),
+      Data => write!(f, ".data"),
+      Assemble => write!(f, ".ltorg"),
+      Label(l) => write!(f, "{}:", l),
+      Word(n) => write!(f, ".word {}", n),
+      Ascii(s) => write!(f, ".ascii \"{}\"", s),
+    }
+  }
+}
+
+/* ======== Instructions! ======== */
+
+impl Display for Instr {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    use Instr::*;
+    match self {
+      Push => write!(f, "PUSH {{lr}}"),
+
+      Pop => write!(f, "POP {{pc}}"),
+
+      Branch(link, label) => {
+        write!(f, "B{} {}", if *link { "L" } else { "" }, label)
+      }
+
+      Store(size, dst, (src, off)) => {
+        write!(f, "STR{} {}, ", size, dst)?;
+        if *off == 0 {
+          write!(f, "{}", src)
+        } else {
+          write!(f, "[{}, #{}]", src, off)
+        }
+      }
+
+      Load(size, dst, (src, off)) => {
+        write!(f, "LDRS{} {}, ", size, dst)?;
+        if *off == 0 {
+          write!(f, "{}", src)
+        } else {
+          write!(f, "[{}, #{}]", src, off)
+        }
+      }
+
+      Binary(instr, dst, src, op2, flags) => {
+        write!(
+          f,
+          "{}{} {}, {}, {}",
+          instr,
+          if *flags { "S" } else { "" },
+          dst,
+          src,
+          op2
+        )
+      }
+
+      Unary(instr, dst, op2, flags) => {
+        write!(
+          f,
+          "{}{} {}, {}",
+          instr,
+          if *flags { "S" } else { "" },
+          dst,
+          op2
+        )
+      }
+
+      Multiply(r1, r2, r3, r4) => write!(f, "SMULL {}, {}, {}, {}", r1, r2, r3, r4),
+    }
+  }
+}
+
+impl Display for DataSize {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    use DataSize::*;
+    match self {
+      Byte => write!(f, "B"),
+      Word => Ok(()),
+    }
+  }
+}
+
+impl Display for UnaryInstr {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    use UnaryInstr::*;
+    let tag = match self {
+      Mov => "MOV",
+      Cmp => "CMP",
+    };
+    write!(f, "{}", tag)
+  }
+}
+
+impl Display for BinaryInstr {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    use BinaryInstr::*;
+    let tag = match self {
+      Add => "ADDS",
+      Sub => "SUB",
+      RevSub => "RSBS",
+      And => "AND",
+      Or => "ORR",
+    };
+    write!(f, "{}", tag)
+  }
+}
+
 impl Display for Op2 {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     use Op2::*;
@@ -87,113 +196,6 @@ impl Display for Reg {
     match self {
       RegNum(num) => write!(f, "r{}", num),
       StackPointer => write!(f, "sp"),
-    }
-  }
-}
-
-impl Display for Directive {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    use Directive::*;
-    match self {
-      Text => write!(f, ".text"),
-      Data => write!(f, ".data"),
-      Assemble => write!(f, ".ltorg"),
-      Label(l) => write!(f, "{}:", l),
-      Word(n) => write!(f, ".word {}", n),
-      Ascii(s) => write!(f, ".ascii \"{}\"", s),
-    }
-  }
-}
-
-impl Display for DataSize {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    use DataSize::*;
-    match self {
-      Byte => write!(f, "B"),
-      Word => Ok(()),
-    }
-  }
-}
-
-impl Display for BinaryInstr {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    use BinaryInstr::*;
-    let tag = match self {
-      Add => "ADDS",
-      Sub => "SUB",
-      RevSub => "RSBS",
-      And => "AND",
-      Or => "ORR",
-    };
-    write!(f, "{}", tag)
-  }
-}
-
-impl Display for UnaryInstr {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    use UnaryInstr::*;
-    let tag = match self {
-      Mov => "MOV",
-      Cmp => "CMP",
-    };
-    write!(f, "{}", tag)
-  }
-}
-
-impl Display for Instr {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    use Instr::*;
-    match self {
-      Push => write!(f, "PUSH {{lr}}"),
-
-      Pop => write!(f, "POP {{pc}}"),
-
-      Branch(link, label) => {
-        write!(f, "B{} {}", if *link { "L" } else { "" }, label)
-      }
-
-      Store(size, dst, (src, off)) => {
-        write!(f, "STR{} {}, ", size, dst)?;
-        if *off == 0 {
-          write!(f, "{}", src)
-        } else {
-          write!(f, "[{}, #{}]", src, off)
-        }
-      }
-
-      Load(size, dst, (src, off)) => {
-        write!(f, "LDRS{} {}, ", size, dst)?;
-        if *off == 0 {
-          write!(f, "{}", src)
-        } else {
-          write!(f, "[{}, #{}]", src, off)
-        }
-      }
-
-      Binary(instr, dst, src, op2, flags) => {
-        write!(
-          f,
-          "{}{} {}, {}, {}",
-          instr,
-          if *flags { "S" } else { "" },
-          dst,
-          src,
-          op2
-        )
-      }
-
-      Unary(instr, dst, op2, flags) => {
-        write!(
-          f,
-          "{}{} {}, {}",
-          instr,
-          if *flags { "S" } else { "" },
-          dst,
-          op2
-        )
-      }
-
-      Multiply(r1, r2, r3, r4) => write!(f, "SMULL {}, {}, {}, {}", r1, r2, r3, r4),
     }
   }
 }
