f1490a329ab1cc755b70053f285c0f35db03a5d2
White, Otto <ow20@ic.ac.uk>
2022-02-25 18:45:56 +0000

    feat: add exit_code test list and generation script, generate exit_code tests

---
+++ test_integration/wacc_examples_exit_codes/generate_test_data.sh
@@ -0,0 +1,45 @@
+generate_our_test_data() {
+  while read in; do 
+
+    input_file=$(echo $in | cut -d, -f1)
+    our_output_file=$(echo $in | cut -d, -f2)
+
+    ../../target/release/wacc_32 $input_file >/dev/null 2>&1
+    echo $? > $our_output_file
+
+  done < ./test_list
+}
+
+generate_their_test_data() {
+  while read in; do 
+    input_file=$(echo $in | cut -d, -f1)
+    their_output_file=$(echo $in | cut -d, -f3)
+
+    valid=`echo $input_file | grep -v 'syntaxErr\|semanticErr' | wc -l`
+    syntaxErr=`echo $input_file | grep syntaxErr | wc -l`
+    semanticErr=`echo $input_file | grep semanticErr | wc -l`
+
+    if [ $valid -eq 1 ] 
+    then
+      exit_code=0
+    elif [ $syntaxErr -eq 1 ] 
+    then
+      exit_code=100
+    elif [ $semanticErr -eq 1 ] 
+    then
+      exit_code=200
+    fi
+
+    echo $exit_code > $their_output_file
+
+  done < ./test_list
+}
+
+if [ "$1" = "ours" ] 
+then
+  generate_our_test_data
+elif [ "$1" = "theirs" ] 
+then
+  generate_their_test_data
+fi
+
