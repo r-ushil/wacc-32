3136687673f015d7d46cf3414b8e14e3661e1c34
White, Otto <ow20@ic.ac.uk>
2022-02-26 17:55:54 +0000

    feat: hide command output from test data generation

--- test_integration/wacc_examples_assembled/generate_test_data.sh
+++ test_integration/wacc_examples_assembled/generate_test_data.sh
@@ -4,7 +4,7 @@ generate_our_test_data() {
     input_file=$(echo $in | cut -d, -f1)
     our_output_file=$(echo $in | cut -d, -f2)
 
-    ../../target/release/wacc_32 $input_file $our_output_file
+    ../../target/release/wacc_32 $input_file $our_output_file >/dev/null 2>&1
 
   done < ./test_list
 }
@@ -14,7 +14,7 @@ generate_their_test_data() {
     input_file=$(echo $in | cut -d, -f1)
     their_output_file=$(echo $in | cut -d, -f3)
 
-    python3 ../apps/refCompileSimple.py $input_file $their_output_file
+    python3 ../apps/refCompileSimple.py $input_file $their_output_file > /dev/null 2>&1
 
   done < ./test_list
 }
