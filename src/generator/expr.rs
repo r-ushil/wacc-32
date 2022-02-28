e6f47b541a9c601780c85d9865b13aca2c0d6543
rp620 <rushil.patel20@imperial.ac.uk>
2022-02-28 16:18:42 +0000

    feat: added match branch in trait in generator/expr.rs and filled out IntLiter

--- src/generator/expr.rs
+++ src/generator/expr.rs
@@ -1,7 +1,29 @@
+use self::CondCode::*;
 use super::*;
+use crate::generator::asm::*;
 
 impl Generatable for Expr {
-  // fn generate(&self, _code: &mut Vec<Instr>, _registers: &[Reg]) {}
+  fn generate(&self, code: &mut GeneratedCode, min_regs: &mut u8) {
+    match self {
+      Expr::IntLiter(val) =>
+      /* LDR r{min_reg}, val */
+      {
+        code.text.push(Asm::Instr(
+          AL,
+          Instr::Load(DataSize::Word, Reg::RegNum(*min_regs), LoadArg::Imm(*val)),
+        ))
+      }
+
+      Expr::BoolLiter(_) => todo!(),
+      Expr::CharLiter(_) => todo!(),
+      Expr::StrLiter(_) => todo!(),
+      Expr::PairLiter => todo!(),
+      Expr::Ident(_) => todo!(),
+      Expr::ArrayElem(_) => todo!(),
+      Expr::UnaryApp(_, _) => todo!(),
+      Expr::BinaryApp(_, _, _) => todo!(),
+    }
+  }
 }
 
 impl Generatable for ArrayElem {
