1bab1cb103edef4e2c30b7187f03fe7628c9d308
Charlie <charlie.lidbury@icloud.com>
2022-03-01 20:04:42 +0000

    fix: global main put after .text in display

--- src/generator/display.rs
+++ src/generator/display.rs
@@ -38,7 +38,7 @@ impl Display for Directive {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     use Directive::*;
     match self {
-      Text => write!(f, ".text"),
+      Text => write!(f, ".text\n.global main"),
       Data => write!(f, ".data"),
       Assemble => write!(f, ".ltorg"),
       Label(l) => write!(f, "{}:", l),
