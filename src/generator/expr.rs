6d6768da43aae6227b2a41d585ef12f6ba544929
White, Otto <ow20@ic.ac.uk>
2022-03-02 09:05:17 +0000

    refactor: extract asm instructions that always execute into a function in expr

--- src/generator/expr.rs
+++ src/generator/expr.rs
@@ -18,31 +18,36 @@ impl Generatable for Expr {
     }
   }
 }
+
 fn assemble_int_liter(code: &mut GeneratedCode, regs: &[Reg], val: &i32) {
   /* LDR r{min_reg}, val */
-  code.text.push(Asm::Instr(
-    AL,
-    Instr::Load(DataSize::Word, regs[0], LoadArg::Imm(*val)),
-  ))
+  code.text.push(always_instruction(Instr::Load(
+    DataSize::Word,
+    regs[0],
+    LoadArg::Imm(*val),
+  )))
 }
 
 fn assemble_bool_liter(code: &mut GeneratedCode, regs: &[Reg], val: &bool) {
   //set imm to 1 or 0 depending on val
   let imm = if *val == true { 1 } else { 0 };
-
   /* MOV r{min_reg}, #imm */
-  code.text.push(Asm::Instr(
-    AL,
-    Instr::Unary(UnaryInstr::Mov, regs[0], Op2::Imm(imm), false),
-  ))
+  code.text.push(always_instruction(Instr::Unary(
+    UnaryInstr::Mov,
+    regs[0],
+    Op2::Imm(imm),
+    false,
+  )))
 }
 
 fn assemble_char_liter(code: &mut GeneratedCode, regs: &[Reg], val: &char) {
   /* MOV r{min_reg}, #'val' */
-  code.text.push(Asm::Instr(
-    AL,
-    Instr::Unary(UnaryInstr::Mov, regs[0], Op2::Char(*val), false),
-  ))
+  code.text.push(always_instruction(Instr::Unary(
+    UnaryInstr::Mov,
+    regs[0],
+    Op2::Char(*val),
+    false,
+  )))
 }
 
 fn assemble_string_liter(code: &mut GeneratedCode, regs: &[Reg], val: &String) {
@@ -62,14 +67,11 @@ fn assemble_string_liter(code: &mut GeneratedCode, regs: &[Reg], val: &String) {
     .push(Asm::Directive(Directive::Ascii(val.clone())));
 
   /* LDR r{min_reg}, ={msg_{msg_no}} */
-  code.text.push(Asm::Instr(
-    AL,
-    Instr::Load(
-      DataSize::Word,
-      regs[0],
-      LoadArg::Label(format!("msg_{}", msg_no)),
-    ),
-  ))
+  code.text.push(always_instruction(Instr::Load(
+    DataSize::Word,
+    regs[0],
+    LoadArg::Label(format!("msg_{}", msg_no)),
+  )))
 }
 
 fn assemble_unary_app(
@@ -104,6 +106,10 @@ fn assemble_binary_app(
   binary_op_gen(op, code, regs[0], regs[1]);
 }
 
+fn always_instruction(instruction: Instr) -> Asm {
+  Asm::Instr(AL, instruction)
+}
+
 fn assemble_temp_default(expr: &Expr, code: &mut GeneratedCode, regs: &[Reg]) {
   code.text.push(Asm::Directive(Directive::Label(format!(
     "{:?}.generate(_, {:?})",
@@ -115,29 +121,23 @@ fn unary_op_gen(unary_op: &UnaryOper, code: &mut GeneratedCode, reg: Reg) {
   match unary_op {
     UnaryOper::Bang => {
       /* EOR reg, reg, #1 */
-      code.text.push(Asm::Instr(
-        AL,
-        Instr::Binary(
-          BinaryInstr::Eor,
-          reg.clone(),
-          reg.clone(),
-          Op2::Imm(1),
-          false,
-        ),
-      ));
+      code.text.push(always_instruction(Instr::Binary(
+        BinaryInstr::Eor,
+        reg.clone(),
+        reg.clone(),
+        Op2::Imm(1),
+        false,
+      )));
     }
     UnaryOper::Neg => {
       /* RSBS reg, reg, #0 */
-      code.text.push(Asm::Instr(
-        AL,
-        Instr::Binary(
-          BinaryInstr::RevSub,
-          reg.clone(),
-          reg.clone(),
-          Op2::Imm(0),
-          false,
-        ),
-      ));
+      code.text.push(always_instruction(Instr::Binary(
+        BinaryInstr::RevSub,
+        reg.clone(),
+        reg.clone(),
+        Op2::Imm(0),
+        false,
+      )));
     }
     // UnaryOper::Len => {
     //   /* LDR r4, [sp, #4]
@@ -163,30 +163,32 @@ fn binary_op_gen(bin_op: &BinaryOper, code: &mut GeneratedCode, reg1: Reg, reg2:
   match bin_op {
     BinaryOper::Mul => {
       /* SMULL r4, r5, r4, r5 */
-      code.text.push(Asm::Instr(
-        AL,
-        Instr::Multiply(reg1.clone(), reg2.clone(), reg1.clone(), reg2.clone()),
-      ));
+      code.text.push(always_instruction(Instr::Multiply(
+        reg1.clone(),
+        reg2.clone(),
+        reg1.clone(),
+        reg2.clone(),
+      )));
 
       /* CMP r5, r4, ASR #31 */
-      code.text.push(Asm::Instr(
-        AL,
-        Instr::Unary(
-          UnaryInstr::Cmp,
-          reg2.clone(),
-          Op2::Reg(reg1.clone(), 31),
-          false,
-        ),
-      ));
+      code.text.push(always_instruction(Instr::Unary(
+        UnaryInstr::Cmp,
+        reg2.clone(),
+        Op2::Reg(reg1.clone(), 31),
+        false,
+      )));
     }
     BinaryOper::Div => binary_div_mod(BinaryOper::Div, code, reg1, reg2),
     BinaryOper::Mod => binary_div_mod(BinaryOper::Mod, code, reg1, reg2),
     BinaryOper::Add => {
       /* ADDS r4, r4, r5 */
-      code.text.push(Asm::Instr(
-        AL,
-        Instr::Binary(BinaryInstr::Add, dst, reg1, Op2::Reg(reg2, 0), true),
-      ));
+      code.text.push(always_instruction(Instr::Binary(
+        BinaryInstr::Add,
+        dst,
+        reg1,
+        Op2::Reg(reg2, 0),
+        true,
+      )));
       //set overflow error branch to true
       code.predefs.overflow_err = true;
       /* BLVS p_throw_overflow_error */
@@ -197,10 +199,13 @@ fn binary_op_gen(bin_op: &BinaryOper, code: &mut GeneratedCode, reg1: Reg, reg2:
     }
     BinaryOper::Sub => {
       /* SUBS r4, r4, r5 */
-      code.text.push(Asm::Instr(
-        AL,
-        Instr::Binary(BinaryInstr::Sub, dst, reg1, Op2::Reg(reg2, 0), true),
-      ));
+      code.text.push(always_instruction(Instr::Binary(
+        BinaryInstr::Sub,
+        dst,
+        reg1,
+        Op2::Reg(reg2, 0),
+        true,
+      )));
       //set overflow error branch to true
       code.predefs.overflow_err = true;
       /* BLVS p_throw_overflow_error */
@@ -217,17 +222,23 @@ fn binary_op_gen(bin_op: &BinaryOper, code: &mut GeneratedCode, reg1: Reg, reg2:
     BinaryOper::Neq => binary_comp_ops(NE, EQ, code, reg1, reg2),
     BinaryOper::And => {
       /* AND r4, r4, r5 */
-      code.text.push(Asm::Instr(
-        AL,
-        Instr::Binary(BinaryInstr::And, dst, reg1, Op2::Reg(reg2, 0), true),
-      ));
+      code.text.push(always_instruction(Instr::Binary(
+        BinaryInstr::And,
+        dst,
+        reg1,
+        Op2::Reg(reg2, 0),
+        true,
+      )));
     }
     BinaryOper::Or => {
       /* ORR r4, r4, r5 */
-      code.text.push(Asm::Instr(
-        AL,
-        Instr::Binary(BinaryInstr::Or, dst, reg1, Op2::Reg(reg2, 0), true),
-      ));
+      code.text.push(always_instruction(Instr::Binary(
+        BinaryInstr::Or,
+        dst,
+        reg1,
+        Op2::Reg(reg2, 0),
+        true,
+      )));
     }
   }
 }
@@ -235,52 +246,60 @@ fn binary_op_gen(bin_op: &BinaryOper, code: &mut GeneratedCode, reg1: Reg, reg2:
 fn binary_div_mod(op: BinaryOper, code: &mut GeneratedCode, reg1: Reg, reg2: Reg) {
   if op == BinaryOper::Div {
     /* MOV r0, reg1 */
-    code.text.push(Asm::Instr(
-      AL,
-      Instr::Unary(UnaryInstr::Mov, Reg::RegNum(0), Op2::Reg(reg1, 0), true),
-    ));
+    code.text.push(always_instruction(Instr::Unary(
+      UnaryInstr::Mov,
+      Reg::RegNum(0),
+      Op2::Reg(reg1, 0),
+      true,
+    )));
     /* MOV r1, reg2 */
-    code.text.push(Asm::Instr(
-      AL,
-      Instr::Unary(UnaryInstr::Mov, Reg::RegNum(1), Op2::Reg(reg2, 0), true),
-    ));
+    code.text.push(always_instruction(Instr::Unary(
+      UnaryInstr::Mov,
+      Reg::RegNum(1),
+      Op2::Reg(reg2, 0),
+      true,
+    )));
 
     /* BL p_check_divide_by_zero */
     code.predefs.div_by_zero = true;
-    code.text.push(Asm::Instr(
-      AL,
-      Instr::Branch(true, String::from("p_check_divide_by_zero")),
-    ));
+    code.text.push(always_instruction(Instr::Branch(
+      true,
+      String::from("p_check_divide_by_zero"),
+    )));
 
     /* BL __aeabi_idiv */
-    code.text.push(Asm::Instr(
-      AL,
-      Instr::Branch(true, String::from("__aeabi_idiv")),
-    ));
+    code.text.push(always_instruction(Instr::Branch(
+      true,
+      String::from("__aeabi_idiv"),
+    )));
   } else if op == BinaryOper::Mod {
     /* MOV r0, reg1 */
-    code.text.push(Asm::Instr(
-      AL,
-      Instr::Unary(UnaryInstr::Mov, Reg::RegNum(0), Op2::Reg(reg1, 0), true),
-    ));
+    code.text.push(always_instruction(Instr::Unary(
+      UnaryInstr::Mov,
+      Reg::RegNum(0),
+      Op2::Reg(reg1, 0),
+      true,
+    )));
     /* MOV r1, reg2 */
-    code.text.push(Asm::Instr(
-      AL,
-      Instr::Unary(UnaryInstr::Mov, Reg::RegNum(1), Op2::Reg(reg2, 0), true),
-    ));
+    code.text.push(always_instruction(Instr::Unary(
+      UnaryInstr::Mov,
+      Reg::RegNum(1),
+      Op2::Reg(reg2, 0),
+      true,
+    )));
 
     /* BL p_check_divide_by_zero */
     code.predefs.div_by_zero = true;
-    code.text.push(Asm::Instr(
-      AL,
-      Instr::Branch(true, String::from("p_check_divide_by_zero")),
-    ));
+    code.text.push(always_instruction(Instr::Branch(
+      true,
+      String::from("p_check_divide_by_zero"),
+    )));
 
     /* BL __aeabi_idivmod */
-    code.text.push(Asm::Instr(
-      AL,
-      Instr::Branch(true, String::from("__aeabi_idivmod")),
-    ));
+    code.text.push(always_instruction(Instr::Branch(
+      true,
+      String::from("__aeabi_idivmod"),
+    )));
   } else {
     unreachable!("undefined!");
   }
@@ -294,15 +313,12 @@ fn binary_comp_ops(
   reg2: Reg,
 ) {
   /* CMP r4, r5 */
-  code.text.push(Asm::Instr(
-    AL,
-    Instr::Unary(
-      UnaryInstr::Cmp,
-      reg1.clone(),
-      Op2::Reg(reg2.clone(), 0),
-      false,
-    ),
-  ));
+  code.text.push(always_instruction(Instr::Unary(
+    UnaryInstr::Cmp,
+    reg1.clone(),
+    Op2::Reg(reg2.clone(), 0),
+    false,
+  )));
 
   /* MOV{cond1} reg1, #1 */
   code.text.push(Asm::Instr(
