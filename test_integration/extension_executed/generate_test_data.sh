9377de24084981c484ab39a83b768d128a4f6e99
Charlie <charlie.lidbury@icloud.com>
2022-03-10 16:30:41 +0000

    test: integration tests for structs

--- test_integration/extension_executed/generate_test_data.sh
+++ test_integration/extension_executed/generate_test_data.sh
@@ -6,10 +6,13 @@ generate_our_test_data() {
     our_s_file="$our_output_file.s"
 
     mkdir -p $(dirname $our_output_file)
+
     ../../target/release/wacc_32 $input_file $our_s_file >/dev/null 2>&1
+    echo Compiler exit code: $? > $our_output_file
+
     arm-linux-gnueabihf-gcc -static -o $our_output_file.bin -mcpu=arm1176jzf-s -mtune=arm1176jzf-s $our_s_file
 
-    echo --------- stdout --------- > $our_output_file
+    echo --------- stdout --------- >> $our_output_file
     qemu-arm-static -L /usr/arm-linux-gnueabihf/ $our_output_file.bin < inputs.txt >> $our_output_file
     exit_code=$(echo $?)
     echo -------------------------- >> $our_output_file
