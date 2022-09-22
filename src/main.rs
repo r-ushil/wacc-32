b0dcf938cb14f612dd3094cd0456c6b176527684
r-ushil <rushil.patel@cub3.com>
2022-09-22 14:24:17 +0000

    style: printed both outputs in main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use wacc_32::compile;
 
 fn main() {
-  let (terminal_output, _asm) = compile();
-  println!("{}", terminal_output);
+  let (terminal_output, asm) = compile();
+  println!("{}\n{}", terminal_output, asm);
 }
