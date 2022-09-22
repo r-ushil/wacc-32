b53610b2609bbc53cef5b5dc84df884a1c3ca723
r-ushil <rushil.patel@cub3.com>
2022-09-22 15:03:34 +0000

    build: re-build wasm-pack

--- pkg/wacc_32.d.ts
+++ pkg/wacc_32.d.ts
@@ -1,9 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
+* @param {string} program
+* @param {boolean} analysis_only
 * @returns {CompileResult}
 */
-export function compile(): CompileResult;
+export function compile(program: string, analysis_only: boolean): CompileResult;
 /**
 */
 export class CompileResult {
--- pkg/wacc_32_bg.js
+++ pkg/wacc_32_bg.js
@@ -27,11 +27,72 @@ function getInt32Memory0() {
     }
     return cachedInt32Memory0;
 }
+
+let WASM_VECTOR_LEN = 0;
+
+const lTextEncoder = typeof TextEncoder === 'undefined' ? (0, module.require)('util').TextEncoder : TextEncoder;
+
+let cachedTextEncoder = new lTextEncoder('utf-8');
+
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
+
+function passStringToWasm0(arg, malloc, realloc) {
+
+    if (realloc === undefined) {
+        const buf = cachedTextEncoder.encode(arg);
+        const ptr = malloc(buf.length);
+        getUint8Memory0().subarray(ptr, ptr + buf.length).set(buf);
+        WASM_VECTOR_LEN = buf.length;
+        return ptr;
+    }
+
+    let len = arg.length;
+    let ptr = malloc(len);
+
+    const mem = getUint8Memory0();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = realloc(ptr, len, len = offset + arg.length * 3);
+        const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
+        const ret = encodeString(arg, view);
+
+        offset += ret.written;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
 /**
+* @param {string} program
+* @param {boolean} analysis_only
 * @returns {CompileResult}
 */
-export function compile() {
-    const ret = wasm.compile();
+export function compile(program, analysis_only) {
+    const ptr0 = passStringToWasm0(program, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ret = wasm.compile(ptr0, len0, analysis_only);
     return CompileResult.__wrap(ret);
 }
 
