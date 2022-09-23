5799a325ebe920afe91258992908bfc295d94a2b
r-ushil <rushil.patel@cub3.com>
2022-09-23 15:42:26 +0100

    fix: reinstalled wasm-pack and rebuilt using rustup, fixed type error

--- pkg/wacc_32.d.ts
+++ pkg/wacc_32.d.ts
@@ -17,3 +17,38 @@ export class CompileResult {
 */
   readonly terminal_output: string;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_compileresult_free: (a: number) => void;
+  readonly compileresult_terminal_output: (a: number, b: number) => void;
+  readonly compileresult_asm_output: (a: number, b: number) => void;
+  readonly compile: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
